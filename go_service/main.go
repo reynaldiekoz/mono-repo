2afda4ea91b594f796f996c204bf0ad8284440be
unknown <reynaldieko@gmail.com>
2023-12-28 18:21:45 +0700

    Simple app for both service

---
+++ go_service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1 style='color: #007BFF;'>Hello from Go Service!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Go Service is running on :6011")
+	http.ListenAndServe(":6011", nil)
+}
---
+++ node.js_service/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1 style="color: #28A745;">Hello from Node.js Service!</h1>');
+});
+
+const PORT = process.env.PORT || 6012;
+app.listen(PORT, () => {
+  console.log(`Node.js Service is running on port ${PORT}`);
+});
