8d603381284fdfc9c1ef26c384eeb8e1ec863f23
unknown <reynaldieko@gmail.com>
2023-12-31 13:09:28 +0700

    v5 test

--- go_service/main.go
+++ go_service/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1 style='color: #007BFF;'>Hello from Go Service! V4</h1>")
+	fmt.Fprintf(w, "<h1 style='color: #007BFF;'>Hello from Go Service! V5</h1>")
 }
 
 func main() {
